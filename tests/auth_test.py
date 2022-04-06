835baa7796bcb8ebadef4666ab6f5e26872eada6
SPowers123 <79128221+SPowers123@users.noreply.github.com>
2022-04-06 01:10:25 -0400

    updated test info

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -22,44 +22,45 @@ def test_auth_pages(client):
     response = client.get("/login")
     assert response.status_code == 200
 
-# Password Confirmation - Unit Test 4
+# Test 4 Password check
 def test_register_matchingPasswords(client):
     #Test that mismatching passwords are correctly handled
-    response = client.post("/register", data={"email": "admin@mail.com", "password": "a", "confirm": ""})
+    response = client.post("/register", data={"email": "test@mail.com", "password": "testpassword", "confirm": ""})
     assert b'Passwords must match' in response.data
     # Test that mismatching passwords are correctly handled even if the field is not empty
-    response = client.post("/register", data={"email": "admin@mail.com", "password": "a", "confirm": "b"})
+    response = client.post("/register", data={"email": "test@mail.com", "password": "testpassword", "confirm": "test2"})
     assert b'Passwords must match' in response.data
 
-# Successful Registration - Unit Test 8
+# Test 8, does registration work
 def test_register(client):
     assert client.get("/register").status_code == 200
-    response = client.post("/register", data={"email": "admin@mail.com", "password": "abcdef", "confirm": "abcdef"})
+    response = client.post("/register", data={"email": "test@mail.com", "password": "testpassword", "confirm": "testpassword"})
     with client.application.app_context():
-        user_id = User.query.filter_by(email="admin@mail.com").first().get_id()
+        user_id = User.query.filter_by(email="test@mail.com").first().get_id()
     #check if the user is redirected properly
     assert "/login" == response.headers["Location"]
     #check if the user is in the database
     assert user_id is not None
 
-# Successful Login - Unit Test 7
+# Test 7, does login work
 def test_login(client):
     with client:
         assert client.get("/login").status_code == 200
-        response = client.post("/login", data={"email": "admin@mail.com", "password": "abcdef", "confirm": "abcdef"})
+        response = client.post("/login", data={"email": "test@mail.com", "password": "testpassword", "confirm": "testpassword"})
         #test that the user is redirected to the dashboard
         assert "/dashboard" == response.headers["Location"]
         #test that the session has the correct user id
         with client.application.app_context():
-            user_id = User.query.filter_by(email="admin@mail.com").first().get_id()
+            user_id = User.query.filter_by(email="test@mail.com").first().get_id()
         assert session['_user_id'] == user_id
 
-#Bad password (Login), Bad username/email (Login) - Unit tests 1 and 2
+#Tests 1 and 2, bad password and username/email for login
+
 @pytest.mark.parametrize(
     ("email", "password"),
     (
         ("EmailNotInDatabase", "PasswordNotInDatabase"),
-        ("admin@mail.com", "PasswordNotInDatabase")
+        ("test@mail.com", "PasswordNotInDatabase")
     )
 )
 def test_badLogin(client, email, password):
@@ -68,7 +69,7 @@ def test_badLogin(client, email, password):
     #if the login is invalid, the user should remain on the login page
     assert "/login" == response.headers["Location"]
 
-#Bad username (registration), Bad Password (does not meet criteria; registration) - Unit test 3 and 5
+#tests 3 and 5, bad username and email for registration + bad password criteria
 @pytest.mark.parametrize(
     ("email", "password", "confirm"),
     (
@@ -83,27 +84,27 @@ def test_badRegistration(client, email, password, confirm):
     #if invalid registration data is inputted, the user should return to the registration page
     assert "/login" == response.headers["Location"]
 
-#Test if a user is already registered - Unit test 6
+#Test 6, is the user already registered
 def test_alreadyRegistered(client):
     with client:
         assert client.get("/register").status_code == 200
-        response = client.post("/register", data={"email": "admin@mail.com", "password": "abcdef", "confirm": "abcdef"})
+        response = client.post("/register", data={"email": "test@mail.com", "password": "testpassword", "confirm": "testpassword"})
         assert client.get("/register").status_code == 200
-        response_2 = client.post("/register", data={"email": "admin@mail.com", "password": "abcdef", "confirm": "abcdef"})
+        response_2 = client.post("/register", data={"email": "test@mail.com", "password": "testpassword", "confirm": "testpassword"})
         #The user should now be redirected back to the registration
         assert "/login" == response_2.headers["Location"]
 
 
-#Access the dashboard for logged in users - Unit test 10
+#test 10, acess to dashboard for logged in users
 def test_dashboardAccess(client):
     assert client.get("/login").status_code == 200
-    response = client.post("/login", data={"email": "admin@mail.com", "password": "abcdef", "confirm": "abcdef"})
+    response = client.post("/login", data={"email": "test@mail.com", "password": "testpassword", "confirm": "testpassword"})
     #test that the user is redirected to the dashboard
     assert "/dashboard" == response.headers["Location"]
     #test that the dashboard loads
     assert client.get("/dashboard").status_code == 200
 
-#Deny Access to the dashboard for users not logged in - Unit test 9
+#test 9, deny dashboard to users not logged in
 def test_dashboardDenied(client):
     response = client.get("/dashboard")
     #users not logged in should be redirected to the login
